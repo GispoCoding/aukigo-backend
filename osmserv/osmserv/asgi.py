1a56c7bd9914a662c8f3c630a39c625f92d05676
Joonalai <joona@gispo.fi>
2020-06-16 12:55:32 +0300

    #6: Add initial django source files

---
+++ osmserv/osmserv/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for osmserv project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'osmserv.settings')
+
+application = get_asgi_application()
