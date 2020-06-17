41a1bd77ea04192757cf158750e0b1bc7e001a8a
Joonalai <joona@gispo.fi>
2020-06-17 12:53:21 +0300

    #6: Add geodjango to installed apps

--- osmserv/osmserv/settings.py
+++ osmserv/osmserv/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    # Own apps
 ]
 
 MIDDLEWARE = [
